ec8cc71191f0967754d13984954c51b3f2c201fe
Qblue616 <Quantum717@protonmail.com>
2026-05-25 20:48:41 -0400

    Update build_site.sh

--- build_site.sh
+++ build_site.sh
@@ -64,6 +64,6 @@ buildPlugin()
   echo "" >> "$outdir"/index.yml
 }
 
-find ./plugins -mindepth 2 -name "*.yml" | while read file; do
+find ./plugins -mindepth 1 -name "*.yml" | while read file; do
   buildPlugin "$file"
 done
